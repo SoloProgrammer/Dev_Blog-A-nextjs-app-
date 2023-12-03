79c9869fb6a8d620384a00ce53764b925b49d058
SoloProgrammer <SoloProgrammer>
2023-12-03 13:57:48 +0530

    small changes

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,48 +1,49 @@
 export const server = {
     URL: {
-        local: "http://localhost:3000"
+        local: "http://localhost:3000",
+        production: "dev-blog-a-nextjs-3p5i6t99j-solo-programmers-projects.vercel.app"
     }
 }
 
 export const api = {
     getCategories: () => {
-        return `${server.URL.local}/api/categories`
+        return `${server.URL.production}/api/categories`
     },
     getPosts: (query) => {
-        return `${server.URL.local}/api/posts${query}`
+        return `${server.URL.production}/api/posts${query}`
     },
     getSinglePost: (slug) => {
-        return `${server.URL.local}/api/posts/${slug}`
+        return `${server.URL.production}/api/posts/${slug}`
     },
     savePost: (slug) => {
-        return `${server.URL.local}/api/posts/${slug}/save`
+        return `${server.URL.production}/api/posts/${slug}/save`
     },
     getPostComments: (query) => {
-        return `${server.URL.local}/api/comments${query}`
+        return `${server.URL.production}/api/comments${query}`
     },
     createNewComment: () => {
-        return `${server.URL.local}/api/comments`
+        return `${server.URL.production}/api/comments`
     },
     deleteComment: (query) => {
-        return `${server.URL.local}/api/comments${query}`
+        return `${server.URL.production}/api/comments${query}`
     },
     updateComment: (query) => {
-        return `${server.URL.local}/api/comments${query}`
+        return `${server.URL.production}/api/comments${query}`
     },
     getUser: () => {
-        return `${server.URL.local}/api/user`
+        return `${server.URL.production}/api/user`
     },
     addReply: (commentId) => {
-        return `${server.URL.local}/api/comments/${commentId}/reply`
+        return `${server.URL.production}/api/comments/${commentId}/reply`
     },
     getReplies: (commentId) => {
-        return `${server.URL.local}/api/comments/${commentId}/reply`
+        return `${server.URL.production}/api/comments/${commentId}/reply`
     },
     updateReply: (commentId, query) => {
-        return `${server.URL.local}/api/comments/${commentId}/reply${query}`
+        return `${server.URL.production}/api/comments/${commentId}/reply${query}`
     },
     deleteReply: (commentId, query) => {
-        return `${server.URL.local}/api/comments/${commentId}/reply${query}`
+        return `${server.URL.production}/api/comments/${commentId}/reply${query}`
     }
 }
 
